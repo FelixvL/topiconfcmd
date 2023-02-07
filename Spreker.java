6517a1f5fddb81df9dcf5713f528ca0648df6a9e
G. Bakker <g.bakker@workingspirit.nl>
2023-02-07 13:37:00 +0100

    Verschillende properties en test methode gemaakt

--- Spreker.java
+++ Spreker.java
@@ -1,7 +1,26 @@
 package topiconfcmd;
 
 public class Spreker {
+	private String firstName;
+	private String lastName;
+	private String language;
+	
 	void start(){
 		System.out.println("Spreker start() is aangeroepen.");
+		test();
+	}
+	
+	void test() {
+		System.out.println("\nDe test methode word aangeroepen, maakt een test spreker aan en print info.");
+		Spreker test = new Spreker();
+		test.firstName = "Glenn";
+		test.lastName = "Bakker";
+		test.language = "Nederlands";
+		test.printInfo();
+	}
+	
+	public void printInfo() {
+		System.out.println("Deze spreker heet: " + firstName + " " + lastName);
+		System.out.println("Hij/Zij spreekt de volgende taal: " + language);
 	}
 }
